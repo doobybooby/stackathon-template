f1c00f5fea209bf5a88e550c5d85069d1720c8c0
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-09-18 11:43:39 -0400

    call the newsapi a little differently to allow it on netlify

--- client/store/news.js
+++ client/store/news.js
@@ -5,12 +5,7 @@ const SET_NEWS = 'SET_NEWS'
 const setNews = news => ({type: SET_NEWS, news})
 
 export const getNews = async dispatch => {
-  const response = await axios.get('https://newsapi.org/v2/top-headlines', { 
-    params: {
-      'apiKey': process.env.REACT_APP_API_KEY_NEWSAPI,
-      'country': 'us'
-    }
-  })
+  const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_KEY_NEWSAPI}`)
   return dispatch(setNews(response.data.articles))
 }
 
